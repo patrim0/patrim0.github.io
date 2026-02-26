5cee92b08de97711e03a9b1132a3855ca911e85d
patrim0 <dewk.dev@outlook.com>
2026-02-26 17:39:10 -0500

    mobile view - close hamburger on click

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,23 @@
+import { useRef } from "react";
+import Collapse from "bootstrap/js/dist/collapse";
+
 export default function NavBar() {
+
+    const collapseRef = useRef(null);
+
+    const closeDropDown = () => {
+
+        if (!collapseRef.current) return;
+        if (window.innerWidth >= 768) return;
+
+        const collapse = Collapse.getInstance(collapseRef.current) || new Collapse(collapseRef.current, { toggle: false });
+        collapse.hide();
+    }
 
     return (
         <nav className="navbar navbar-dark navbar-expand-md rounded-pill px-5 floating-nav">
             <div className="container-fluid">
-                <a className="navbar-brand fw-bold" href="#home">PM</a>
+                <a className="navbar-brand fw-bold" href="#home" onClick={closeDropDown}>PM</a>
 
                 <button
                     className="navbar-toggler border-0 shadow-none"
@@ -17,12 +31,12 @@ export default function NavBar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarLinks">
+                <div className="collapse navbar-collapse" id="navbarLinks" ref={collapseRef}>
                     <ul className="navbar-nav ms-auto gap-3">
-                        <li className="nav-item"><a className="nav-link" href="#about">/about</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#projects">/projects</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#technologies">/technologies</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#contact">/contact</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#about" onClick={closeDropDown}>/about</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#projects" onClick={closeDropDown}>/projects</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#technologies" onClick={closeDropDown}>/technologies</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#contact" onClick={closeDropDown}>/contact</a></li>
                     </ul>
                 </div>
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './style.css';
 
 createRoot(document.getElementById('root')!).render(
